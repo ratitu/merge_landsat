1b4dfc7fe34fb14182141acbf5609e12da041ad3
ratitu <rogergodoy@gmail.com>
2019-12-05 14:21:01 -0300

    woking

--- merge.sh
+++ merge.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 for i in $1.TIF
 do 
-	echo $i
+#	echo $i
 	x=$(ls $i|cut -d "_" -f 1-7)
 	echo $i>>$x.merge.txt
 done
 echo $x\_B10.TIF>>$x.merge.txt
 echo $x\_B11.TIF>>$x.merge.txt
+gdal_merge.py -separate -ot Float32 -of GTiff -o $x.gdal.tif --optfile $x.merge.txt
